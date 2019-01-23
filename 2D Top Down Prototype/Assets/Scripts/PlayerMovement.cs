123cc4eaea8a23c0c119644c90fa826c55ab7897
tesdok <leonazred2010@gmail.com>
2019-01-24 00:42:53 +0500

    start

---
+++ 2D Top Down Prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    idle,
+    walk,
+    attack,
+    interact,
+    stagger
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Animator animator;
+    private Vector2 change;
+    private Vector2 cameraPos;
+    private bool NeedMove = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change = Input.mousePosition;
+        //Vector2 mousePos = Input.mousePosition;
+        //cameraPos = Camera.main.ScreenToWorldPoint(change);
+        //change.x = Input.GetAxisRaw("Horizontal"); ;
+        //change.y = Input.GetAxisRaw("Vertical");
+        if ((Input.GetMouseButtonUp(0) == true))
+        {
+            Vector2 mousePos = Input.mousePosition;
+            cameraPos = Camera.main.ScreenToWorldPoint(mousePos);
+            NeedMove = true;
+        }
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack && currentState != PlayerState.stagger)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if ((currentState == PlayerState.walk || currentState == PlayerState.idle) && NeedMove)
+        {
+            UpdateAnimationAndMove();
+        }
+        
+    }
+
+    private IEnumerator AttackCo()
+    {
+        currentState = PlayerState.attack;
+        animator.SetBool("attacking", true);
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (Vector2.Distance(transform.position, cameraPos) > 0.01)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", myRigidbody.transform.);
+            animator.SetFloat("moveY", cameraPos.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+            NeedMove = false;
+        }
+    }
+
+
+    void MoveCharacter()
+    {
+        
+        if(Input.GetButton("nitro"))
+        {
+            StartCoroutine(NitroCo());
+        }
+        else
+        {
+            //myRigidbody.MovePosition(transform.position + change.normalized * speed * Time.deltaTime);
+             myRigidbody.MovePosition(Vector2.MoveTowards(transform.position, cameraPos, speed * Time.deltaTime));
+            
+        }
+    }
+
+    public void Knock(float knockTime)
+    {
+        StartCoroutine(KnockCo(knockTime));
+    }
+
+    private IEnumerator KnockCo(float knockTime)
+    {
+        if (myRigidbody != null)
+        {
+
+            yield return new WaitForSeconds(knockTime);
+            myRigidbody.velocity = Vector2.zero;
+            currentState = PlayerState.idle;
+        }
+    }
+
+    private IEnumerator NitroCo()
+    {
+        //myRigidbody.MovePosition(transform.position + change.normalized * speed * 2 * Time.deltaTime);
+        myRigidbody.MovePosition(Vector2.MoveTowards(transform.position, cameraPos, speed * 2 * Time.deltaTime));
+        yield return new WaitForSeconds(2f);
+        //myRigidbody.MovePosition(transform.position + change.normalized * speed * Time.deltaTime);
+        myRigidbody.MovePosition(Vector2.MoveTowards(transform.position, cameraPos, speed * Time.deltaTime));
+
+    }
+}
